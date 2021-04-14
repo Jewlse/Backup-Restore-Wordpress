515d03af27728be2159c48d02f576eceed5ba47b
Jewlse <julien.dirr@gmail.com>
2021-04-14 10:22:34 +0000

    Modification du script

--- backup-restore-wordpress.py
+++ backup-restore-wordpress.py
@@ -226,7 +226,6 @@ def menu():
 
     # Affichage du menu
 
-#    os.system("clear")
     print("\nSauvegarde et restauration d'un site Wordpress \n\n 1. Sauvegarde de wordpress sur le SFTP \n 2. Restauration de wordpress depuis le SFTP \n 3. Sortir du menu \n")
     choice = input()
 
